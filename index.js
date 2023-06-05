13cf79b1b545a507c0ca875fae6aaa300c3f1534
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-06-05 13:57:14 +0300

    paymeny data saved

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run() {
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
+        const paymentCollection = client.db("bistroDB").collection("payments");
 
 
         app.post('/jwt', (req, res) => {
@@ -192,7 +193,7 @@ async function run() {
         })
 
         // Create a Payment Intent
-        app.post('/create-payment-intent', async (req, res) => {
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
             const { price } = req.body;
             const amount = price * 100;
             const paymentIntent = await stripe.paymentIntents.create({
@@ -205,7 +206,15 @@ async function run() {
             })
         })
 
+        // payment related api
+        app.post('/payments', verifyJWT, async (req, res) => {
+            const payment = req.body;
+            const insertResult = await paymentCollection.insertOne(payment);
+            const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+            const deleteResult = await cartCollection.deleteMany(query)
 
+            res.send({ insertResult, deleteResult });
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
